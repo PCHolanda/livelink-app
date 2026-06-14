53212d7adf8f883b733f953d05d838d230ec35fc
Paulo Cézar <pcfantomas2016@gmail.com>
2026-06-14 00:07:59 -0400

    fix: importar go_router na view de gerencia de usuarios

--- lib/views/admin/user_management_view.dart
+++ lib/views/admin/user_management_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:go_router/go_router.dart';
 import '../../models/user_model.dart';
 import '../../services/supabase_service.dart';
 
